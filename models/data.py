906c3185893970a38612dc2eadc4821970bd819c
Herve_win <rv.monnier@free.fr>
2022-05-30 11:47:49 +0200

    ajouts class

---
+++ models/data.py
@@ -0,0 +1,91 @@
+players = []
+players.append(
+    {
+        "last_name": "Dupont",
+        "first_name": "Eric",
+        "birthday_date": "01/01/1970",
+        "gender": "Man",
+        "ranking": 1,
+    }
+)
+players.append(
+    {
+        "last_name": "Durand",
+        "first_name": "Alain",
+        "birthday_date": "02/02/1975",
+        "gender": "Man",
+        "ranking": 2,
+    }
+)
+players.append(
+    {
+        "last_name": "Duchemin",
+        "first_name": "Simone",
+        "birthday_date": "03/03/1970",
+        "gender": "Woman",
+        "ranking": 3,
+    }
+)
+players.append(
+    {
+        "last_name": "Duval",
+        "first_name": "Thierry",
+        "birthday_date": "04/04/1975",
+        "gender": "Man",
+        "ranking": 4,
+    }
+)
+players.append(
+    {
+        "last_name": "Vincens",
+        "first_name": "Pierre",
+        "birthday_date": "05/05/1970",
+        "gender": "Man",
+        "ranking": 5,
+    }
+)
+players.append(
+    {
+        "last_name": "Courant",
+        "first_name": "Stéphanie",
+        "birthday_date": "06/06/1975",
+        "gender": "Woman",
+        "ranking": 6,
+    }
+)
+players.append(
+    {
+        "last_name": "Point",
+        "first_name": "Géraldine",
+        "birthday_date": "07/07/1970",
+        "gender": "Woman",
+        "ranking": 7,
+    }
+)
+players.append(
+    {
+        "last_name": "Moreau",
+        "first_name": "Lionel",
+        "birthday_date": "08/08/1975",
+        "gender": "Man",
+        "ranking": 8,
+    }
+)
+players.append(
+    {
+        "last_name": "Valleau",
+        "first_name": "Yvelise",
+        "birthday_date": "09/09/1970",
+        "gender": "Woman",
+        "ranking": 9,
+    }
+)
+players.append(
+    {
+        "last_name": "Nadal",
+        "first_name": "Raphaël",
+        "birthday_date": "10/10/1975",
+        "gender": "Man",
+        "ranking": 10,
+    }
+)
--- models/models.py
+++ models/models.py
@@ -1,8 +1,18 @@
+from operator import ge
+
+
 class Tournaments:
     """to create an tournament instance"""
 
     def __init__(
-        self, name, location, date, players_list, description, rounds_number=4
+        self,
+        name,
+        location,
+        date,
+        players_list,
+        description,
+        time_control,
+        rounds_number=4,
     ):
         self.name = name
         self.location = location
@@ -17,6 +27,62 @@ class Tournaments:
 class Players:
     """to create a player instance"""
 
-    def __init__(self, last_name, first_name, birth_date, gender, ranking):
+    def __init__(self, last_name, first_name, birthday_date, gender, ranking):
         self.last_name = last_name
         self.first_name = first_name
+        self.birthday_date = birthday_date
+        self.gender = gender
+        self.ranking = ranking
+
+
+import datetime
+
+
+class Rounds:
+    """description of a round"""
+
+    def __init__(self, matches_list, name):
+        self.matches_list = matches_list
+        self.name = name
+        self.start_time_round = datetime.datetime()
+        self.end_time_round = datetime.datetime()
+
+    def pairs_generation(self):
+        pass
+
+
+class Matches:
+    def __init__(
+        self, first_player, second_player, result_first_player, result_second_player
+    ):
+        pass
+
+
+import data
+import random
+
+if __name__ == "__main__":
+
+    index_list_players = random.sample(range(len(data.players)), 8)
+    players_list = []
+    for index_player in index_list_players:
+        players_list.append(
+            Players(
+                last_name=data.players[index_player]["last_name"],
+                first_name=data.players[index_player]["first_name"],
+                birthday_date=data.players[index_player]["birthday_date"],
+                gender=data.players[index_player]["gender"],
+                ranking=data.players[index_player]["ranking"],
+            )
+        )
+
+    tournament = Tournaments(
+        "tournament_1",
+        "Bordeaux",
+        "30/05/2022",
+        players_list,
+        "Nice day to play",
+        "bullet",
+    )
+    for e in tournament.players_list:
+        print(e.last_name)
